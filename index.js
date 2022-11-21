a53e9b12a2b2fd76c6f3e7d9e3d3fad8571aa85c
xochitl-b <xochitl.burtnik@gmail.com>
2022-11-21 13:30:11 +0000

    updating js file

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const   http = require('http'), //HTTP server
+        path = require('path'),
         express = require('express'), //Handling HTTP requests & routing
         fs = require('fs'), //File system functionalities
         xmlParse = require('xslt-processor').xmlParse, //XML handling
         xsltProcess = require('xslt-processor').xsltProcess, //XSLT handling
         router = express(), //Init our router
         server = http.createServer(router); //Init our server
+        
+        router.use(express.static(path.resolve(__dirname,'views'))); 
 
 router.get('/', function(req, res) {
 
